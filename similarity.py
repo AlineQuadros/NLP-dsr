34361b05439595aeef83e93410eee3ed88a002e7
AlineQuadros <bioaline@gmail.com>
2019-11-30 10:52:44 +0100

    fixed bug when saving lists

--- similarity.py
+++ similarity.py
@@ -8,7 +8,7 @@ Functions to calculate similarity between abstracts
 
 """
 ##############################################################################
-
+import pandas as pd
 import numpy as np
 import tensorflow_hub as hub
 from nltk import tokenize
@@ -18,33 +18,18 @@ import config
 # load the USE - universal-sentence-encoder from Google
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/3")
 
+clean_dataset = pd.read_csv('data/clean_dataset.csv')
 calculate_overall_USE(clean_dataset)
 
 
 ##############################################################################
-# def calculate_overall_USE(data):
-#     for i in range(len(data)):
-#         # calculate similarity ALL vs. ALL
-#         print(i)
-#         list_similarity_USE = []
-#         for j in np.arange(i + 1, len(data.abstract)):
-#             ab1 = tokenize.sent_tokenize(data.abstract[i])
-#             ab2 = tokenize.sent_tokenize(data.abstract[j])
-#             e1 = embed(ab1)["outputs"]
-#             e2 = embed(ab2)["outputs"]
-#             list_similarity_USE.append([i, j, get_similarity_pairs_USE(e1, e2)])
-#         # update the file at every completed j
-#         list_similarity_USE = pd.DataFrame(list_similarity_USE)
-#         try:
-#             list_similarity_USE.to_csv(config.SIMILARITY_USE_PATH, mode='a', index=False, header=False)
-#         except:
-#             print('error at i:', i, ' and j:', j)
 
 def calculate_overall_USE(data):
     for i in range(len(data)):
         # calculate similarity ALL vs. ALL
         print(i)
         list_similarity_USE = []
+        list_sim = []
         for j in np.arange(i + 1, len(data)):
             ab1 = tokenize.sent_tokenize(data.abstract[i])
             ab2 = tokenize.sent_tokenize(data.abstract[j])
@@ -52,21 +37,33 @@ def calculate_overall_USE(data):
             e2 = embed(ab2)["outputs"]
             sim = get_similarity_pairs_USE(e1, e2)
             line = "{'ab1':'" + str(data.id[i]) + "','ab2':'" + str(data.id[j]) + "','sim_use':" + str(round(sim, 3)) + "}"
-            temp = 'similarity/' + str(i) + '_' + str(j) + '.csv'
-            with open(temp, 'w+') as f:
-                f.write(line)
-                f.close()
+            list_sim.append(line)
+        temp = 'similarity/iteration' + str(i) + '_' + str(j) + '.csv'
+        with open(temp, 'w+') as f:
+            f.write('\n'.join(list_sim))
+            f.close()
 
 
 def get_similarity_pairs_USE(e1, e2):
+    """ calculates the mean of the maximum similarity between one sentence of one abstract against
+     all sentences of the other abstract"""
     # iterate over the sentences of the first abstract
     list_sims = []
     for s in range(e1.shape[0]):
         max_sim = 0
         # iterate over the sentences of the second abstract
         for s2 in range(e2.shape[0]):
+            # calculate similarity
             sim = np.inner(e1[s], e2[s2])
             if sim > max_sim: max_sim = sim
         list_sims.append(max_sim)
+    for s in range(e2.shape[0]):
+        max_sim = 0
+        # iterate over the sentences of the second abstract
+        for s2 in range(e1.shape[0]):
+            # calculate similarity
+            sim = np.inner(e2[s], e1[s2])
+            if sim > max_sim: max_sim = sim
+        list_sims.append(max_sim)
 
-    return sum(list_sims) / (e1.shape[0])
+    return sum(list_sims)/(len(list_sims))
