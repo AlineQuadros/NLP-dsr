cab948d731eff525beb041440cc08402b1241600
AlineQuadros <bioaline@gmail.com>
2019-12-10 01:39:19 +0100

    app retrieves top similar papers

---
+++ server.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request, render_template
+
+import joblib
+import pandas as pd
+import numpy as np
+from gensim.models import doc2vec
+import os
+
+app = Flask(__name__)
+
+data = pd.read_csv("data/clean_dataset.csv")
+# load pre-saved model
+model_d2v = doc2vec.Doc2Vec.load('models/model.doc2vec')
+# load corpus
+corpus_d2v = joblib.load('corpus/all_abstracts_for_word2vec.cor')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    ids = np.random.randint(0, len(data), 3)
+    abstracts = data.abstract[ids]
+    titles = data.title[ids]
+    arxiv_id = data.id[ids]
+    abstracts.reset_index(drop=True, inplace=True)
+    titles.reset_index(drop=True, inplace=True)
+    arxiv_id.reset_index(drop=True, inplace=True)
+    return render_template('index.html',
+                           abstract1=abstracts[0], abstract2=abstracts[1], abstract3=abstracts[2],
+                           title1=titles[0], title2=titles[1], title3=titles[2],
+                           id1 = arxiv_id[0], id2 = arxiv_id[1], id3 = arxiv_id[2]) #**locals()
+
+
+@app.route('/choose', methods=['GET', 'POST'])
+def load_similar():
+    arxivid = request.form['chosen_id']
+    target = arxivid_to_numid(arxivid)
+    list_sim = get_top_similar_word2vec(target)
+    result_slice = data[data.id.isin(list_sim)]
+    result_slice.reset_index(drop=True, inplace=True)
+    return render_template('index.html',
+                           sug_abstract1=result_slice.abstract[1],
+                           sug_abstract2=result_slice.abstract[2],
+                           sug_abstract3=result_slice.abstract[3],
+                           sug_abstract4=result_slice.abstract[4],
+                           sug_abstract5=result_slice.abstract[5],)
+
+def get_top_similar_word2vec(num_id):
+    list_sim = []
+    inferred_vector = model_d2v.infer_vector(corpus_d2v[num_id].words)
+    sims = model_d2v.docvecs.most_similar([inferred_vector], topn=6)
+    for abs in sims:
+        list_sim.append(numid_to_arxivid(abs[0]))
+    return list_sim
+
+def numid_to_arxivid(num_id):
+    return data.id[num_id]
+
+def arxivid_to_numid(arxiv_id):
+    return data[data.id == arxiv_id].index[0]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
