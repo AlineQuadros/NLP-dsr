3bb1d6136a06077d88e6ede2bd026d551d20d0da
AlineQuadros <bioaline@gmail.com>
2019-12-12 22:38:44 +0100

    fixed passing the results

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ model_d2v = doc2vec.Doc2Vec.load('models/doc2vec_all_abstracts.model')
 # load corpus doc2vec
 corpus_d2v = joblib.load('corpus/all_abstracts_for_doc2vec.corpus')
 # Load LDA based on the titles
-lda = LdaModel.load('models/LDA_titles_30topics')
+lda = LdaModel.load('models/LDA_titles_30topics.model')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -27,28 +27,27 @@ def index():
     abstracts.reset_index(drop=True, inplace=True)
     titles.reset_index(drop=True, inplace=True)
     arxiv_id.reset_index(drop=True, inplace=True)
-    return render_template('page1.html',
+    return render_template('index.html',
                            abstract1=abstracts[0], abstract2=abstracts[1], abstract3=abstracts[2],
                            title1=titles[0], title2=titles[1], title3=titles[2],
                            id1 = arxiv_id[0], id2 = arxiv_id[1], id3 = arxiv_id[2]) #**locals()
 
 
-@app.route('/choose', methods=['GET', 'POST'])
+@app.route('/results', methods=['GET', 'POST'])
 def load_similar():
-    arxivid = request.form['chosen_id']
+    arxivid = request.form['button']
     target = arxivid_to_numid(arxivid)
-    list_sim = get_top_similar_word2vec(target)
+    list_sim = get_top_similar_doc2vec(target)
     result_slice = data[data.id.isin(list_sim)]
     result_slice.reset_index(drop=True, inplace=True)
-    return render_template('index.html',
-                           abstract1=data.abstract[target], title1=data.title[target],  id1 = arxivid,
-                           sug_abstract1=result_slice.abstract[1],
-                           sug_abstract2=result_slice.abstract[2],
-                           sug_abstract3=result_slice.abstract[3],
-                           sug_abstract4=result_slice.abstract[4],
-                           sug_abstract5=result_slice.abstract[5])
+    return render_template('results.html',
+                           results=result_slice,
+                           abstract1=data.abstract[target],
+                           title1=data.title[target],
+                           id1 = arxivid)
+     #return render_template('results.html', **locals())
 
-def get_top_similar_word2vec(num_id):
+def get_top_similar_doc2vec(num_id):
     list_sim = []
     inferred_vector = model_d2v.infer_vector(corpus_d2v[num_id].words)
     sims = model_d2v.docvecs.most_similar([inferred_vector], topn=6)
